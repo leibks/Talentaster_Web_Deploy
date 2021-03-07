2cd704c998a40712dd55c87bf482a2457cc31bf8
bowenleis <lei.b@husky.neu.edu>
2021-03-06 23:36:43 -0500

    version1

---
+++ build/precache-manifest.ceea53ca06d72b01ca9b60467db86728.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d9a306af4941c5c6c693302956c01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43f8a7659792c57c4912",
+    "url": "/static/css/2.301005e7.chunk.css"
+  },
+  {
+    "revision": "1f9aaef90dd8f097ef5b",
+    "url": "/static/css/main.9b3ac610.chunk.css"
+  },
+  {
+    "revision": "43f8a7659792c57c4912",
+    "url": "/static/js/2.4536d5c1.chunk.js"
+  },
+  {
+    "revision": "47443401be3cfeb14f0c199b46ba616c",
+    "url": "/static/js/2.4536d5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9aaef90dd8f097ef5b",
+    "url": "/static/js/main.5c83cb17.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "b7bb9d0bf61de3ad6821fbbae9707861",
+    "url": "/static/media/logo.b7bb9d0b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
